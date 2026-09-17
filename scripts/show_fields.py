e32cde0b05f86a434b44d567c21eb331364b961e
DeadSno <deadsno1613@gmail.com>
2026-09-17 08:27:09 +0300

    chore: remove temp diagnostic scripts, add to gitignore

--- scripts/show_fields.py
+++
@@ -1,8 +0,0 @@
-﻿import json
-d = json.load(open('docs/data.json', encoding='utf-8'))
-s = d[0]
-print('Добавка:', s.get('name'))
-print()
-print('Все поля:')
-for k, v in s.items():
-    print(f'  {k}: {repr(v)[:80]}')
